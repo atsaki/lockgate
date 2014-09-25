d6cfd4f1835c5eba7974011463f5510a1947f98e
Atsushi Sasaki <atsaki01@gmail.com>
2014-09-25 22:51:28 +0900

    add args to template list command

--- command/template.go
+++ command/template.go
@@ -13,6 +13,31 @@ var (
 	TemplateList = cli.Command{
 		Name: "list",
 		Help: "List templates",
+		Args: []cli.Argument{
+			cli.Argument{
+				Name: "ids",
+				Help: "VM ids",
+				Type: cli.Strings,
+			},
+		},
+		Flags: []cli.Flag{
+			cli.Flag{
+				Name:    "templatefilter",
+				Help:    "templatefilter",
+				Default: "executable",
+				Type:    cli.String,
+			},
+			cli.Flag{
+				Name: "name",
+				Help: "The name of the template",
+				Type: cli.String,
+			},
+			cli.Flag{
+				Name: "zone",
+				Help: "The zoneid or zonename of the template",
+				Type: cli.String,
+			},
+		},
 		Action: func(c *cli.Context) {
 
 			lockgate.SetLogLevel(c)
@@ -22,15 +47,44 @@ var (
 				log.Fatal(err)
 			}
 			params := cloudstack.ListTemplatesParameter{}
-			params.SetTemplatefilter("executable")
-			result, err := client.ListTemplates(params)
-			if err != nil {
-				fmt.Println(err)
-				log.Fatal(err)
+
+			ids := lockgate.GetArgumentsFromStdin()
+			ids = append(ids, c.Command.Arg("ids").Value().([]string)...)
+
+			templatefilter := c.Command.Flag("templatefilter").Value().(string)
+			if templatefilter != "" {
+				params.SetTemplatefilter(templatefilter)
+			}
+			name := c.Command.Flag("name").Value().(string)
+			if name != "" {
+				params.SetName(name)
+			}
+			zone := c.Command.Flag("zone").Value().(string)
+			if zone != "" {
+				params.SetZoneid(zone)
+			}
+
+			var templates []cloudstack.Template
+			if len(ids) > 0 {
+				for _, id := range ids {
+					params.SetId(id)
+					resp, err := client.ListTemplates(params)
+					if err != nil {
+						fmt.Println(err)
+						log.Fatal(err)
+					}
+					templates = append(templates, resp...)
+				}
+			} else {
+				templates, err = client.ListTemplates(params)
+				if err != nil {
+					fmt.Println(err)
+					log.Fatal(err)
+				}
 			}
 
 			w := lockgate.GetTabWriter(c)
-			w.Print(result)
+			w.Print(templates)
 		},
 	}
 
