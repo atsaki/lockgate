cd4f2a35eca28f265a61901135565fcd5d01c646
Atsushi Sasaki <atsaki01@gmail.com>
2014-09-26 11:53:09 +0900

    change default output of api command from json to tabwriter

--- command/api.go
+++ command/api.go
@@ -1,9 +1,11 @@
 package command
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/atsaki/lockgate"
 	"github.com/atsaki/lockgate/cli"
@@ -44,7 +46,49 @@ var (
 				fmt.Fprintln(os.Stderr, err)
 				log.Fatal(err)
 			}
-			lockgate.PrettyPrint(result)
+
+			items := []interface{}{}
+			var key string
+			var value json.RawMessage
+
+			temp := map[string]json.RawMessage{}
+			err = json.Unmarshal(result, &temp)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				log.Fatal(err)
+			}
+			for key, value = range temp {
+				if key != "count" {
+					break
+				}
+			}
+
+			if key == "success" {
+				if string(value) == "true" {
+					items = append(items, map[string]bool{"success": true})
+				} else {
+					items = append(items, map[string]bool{"success": false})
+				}
+			} else if strings.HasPrefix(command, "list") {
+				err = json.Unmarshal(value, &items)
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
+					log.Fatal(err)
+				}
+			} else {
+				var item interface{}
+				err := json.Unmarshal(value, &item)
+				if err != nil {
+					if err != nil {
+						fmt.Fprintln(os.Stderr, err)
+						log.Fatal(err)
+					}
+				}
+				items = append(items, item)
+			}
+
+			w := lockgate.GetTabWriter(c)
+			w.Print(items)
 		},
 	}
 )
